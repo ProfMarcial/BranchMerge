96fc587feab38ca06ce26c3dc0dd08e21bf09e92
Prof. Marcial <marcial@gmail.com>
2026-07-14 20:38:48 -0300

    Primeiro Merge

--- src/branchmerge/BranchMerge.java
+++ src/branchmerge/BranchMerge.java
@@ -15,9 +15,8 @@ public class BranchMerge {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Aqui é a nova Branch que criamos");
-        System.out.println("Teste 100001");
-        System.out.println("Versão Não-Oficial");
+        System.out.println("VERSÃO FINAL 1.1.0");
+        
     }
     
 }
